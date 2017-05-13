60be5e8d4213d42736e52e172db5f229dcfec3ec
陈亚飞 <chenyafei@chenyafeideMacBook-Pro.local>
2017-05-14 07:52:29 +0800

    Initial Commit

---
+++ 知享/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  知享
+//
+//  Created by 陈亚飞 on 2017/5/14.
+//  Copyright © 2017年 陈亚飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
